b2a71bb2d9edd9a3a0e1304a402ce6a0302b25cd
Miguel Durán <miguel.386984@gmail.com>
2025-01-21 07:37:03 -0500

    chore: configure prettier

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application } from "express";
-import helmet from "helmet";
-import compression from "compression";
-import cors from "cors";
-import rateLimit from "express-rate-limit";
-import dotenv from "dotenv";
-import env from "env-var";
+import express, { Application } from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import dotenv from 'dotenv';
+import env from 'env-var';
 
 dotenv.config();
 
@@ -13,24 +13,24 @@ const app: Application = express();
 // Middlewares
 app.use(helmet()); // Configura encabezados de seguridad
 app.use(compression()); // Habilita la compresión para mejorar el rendimiento
-app.use(cors({ origin: env.get("CORS_ORIGIN").asString() || "*" })); // Configura CORS dinámico
+app.use(cors({ origin: env.get('CORS_ORIGIN').asString() || '*' })); // Configura CORS dinámico
 app.use(express.json()); // Manejo de JSON en el cuerpo de las solicitudes
 
 // Límite de solicitudes para prevenir DoS
 const limiter = rateLimit({
   windowMs: env
-    .get("RATE_LIMIT_WINDOW_MS")
+    .get('RATE_LIMIT_WINDOW_MS')
     .default(1 * 60 * 1000)
     .asInt(),
-  max: env.get("RATE_LIMIT_MAX_REQUESTS").default(5).asInt(),
+  max: env.get('RATE_LIMIT_MAX_REQUESTS').default(5).asInt(),
   message:
-    "Demasiadas solicitudes desde esta IP. Por favor, inténtelo más tarde.",
+    'Demasiadas solicitudes desde esta IP. Por favor, inténtelo más tarde.',
 });
 app.use(limiter);
 
 // Rutas de ejemplo
-app.get("/", (_, res) => {
-  res.json({ message: "¡Bienvenido a la API segura!" });
+app.get('/', (_, res) => {
+  res.json({ message: '¡Bienvenido a la API segura!' });
 });
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import app from "./app";
-import env from "env-var";
+import app from './app';
+import env from 'env-var';
 
 // Puerto dinámico desde variables de entorno o por defecto 3000
-const PORT = env.get("PORT").default(3000).asInt();
+const PORT = env.get('PORT').default(3000).asInt();
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
